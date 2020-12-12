87d5f0a9ee999cc0d34e93837607ac06c48ae0e0
Casey <innerforest7@gmail.com>
2020-12-11 23:23:59 -0800

    seperate IntersectionObservers

--- animation.js
+++ animation.js
@@ -1,40 +1,66 @@
 window.addEventListener('DOMContentLoaded', ()=>{
-  const sectionOne = document.querySelector('.networking__text');
-  const sliders = document.querySelectorAll('.slide-in');
-  console.log('sectionOne', sectionOne)
+  // const sectionOne = document.querySelector('.networking__text'); 
 
-  const options = {
-    root: null, //null is defualt setting (is the viewport)
-    threshold: ".25", // 0 is default (0-1) (amount of item that must be on page to fire)
-    margin: "0" // 0 is default (margin on viewport if root is null)(pixels or percentages)
-  }
+  // const options = {
+  //   root: null, //null is defualt setting (is the viewport)
+  //   threshold: ".25", // 0 is default (0-1) (amount of item that must be on page to fire)
+  //   margin: "0" // 0 is default (margin on viewport if root is null)(pixels or percentages)
+  // }
 
-  const observer =
-    new IntersectionObserver(function
-      (entries, observer) {
-      entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-          return;
-        } else {
-          entry.target.classList.toggle("scroll-from-left")
-          console.log(entry)
-          observer.unobserve(entry.target);
-        }
+//   const observer =
+//     new IntersectionObserver(function
+//       (entries, observer) {
+//       entries.forEach(entry => {
+//         if (!entry.isIntersecting) {
+//           return;
+//         } else {
+//           entry.target.classList.toggle("scroll-from-left")
+//           console.log(entry)
+//           observer.unobserve(entry.target);
+//         }
   
-      });
+//       });
  
-}, options);
+// }, options);
 
-  observer.observe(sectionOne)
+//   observer.observe(sectionOne)
 
 
+// ---------slide-in-----------------
+  const sliders = document.querySelectorAll('.slide-in');
+ 
 
-  const faders = document.querySelectorAll('.fade-in');
-
-  const appearOptions = {
-    threshold: 0,
+  const slideOptions = {
+    threshold: .0,
     rootMargin: "300px",
   }
+
+   const slideOnScroll = new IntersectionObserver
+   (function(
+     entries,
+     slideOnScroll
+   ){
+     entries.forEach(entry => {
+       if (!entry.isIntersecting){
+         return
+       } else {
+         entry.target.classList.add('appear');
+         slideOnScroll.unobserve(entry.target);
+       }
+     })
+   },
+   slideOptions);
+
+   sliders.forEach(slider => {
+     slideOnScroll.observe(slider);
+   })
+// --------fade-in------------------------------
+  const appearOptions = {
+    threshold: .5,
+    rootMargin: "300px",
+  }
+
+const faders = document.querySelectorAll('.fade-in');
 
    const appearOnScroll = new IntersectionObserver
    (function(
@@ -51,15 +77,10 @@ window.addEventListener('DOMContentLoaded', ()=>{
      })
    },
    appearOptions);
+
    faders.forEach(fader => {
      appearOnScroll.observe(fader)
    })
-
-   sliders.forEach(slider => {
-     appearOnScroll.observe(slider);
-   })
-
-   
  
 
 
